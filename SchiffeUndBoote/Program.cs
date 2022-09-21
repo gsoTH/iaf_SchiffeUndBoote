7c39e2f2745498889e0366cb6d3a2ae832aa523e
M.Thomas <m.thomas@gso.schule.koeln>
2022-09-21 21:13:00 +0200

    Objekterzeugung eingefügt

--- SchiffeUndBoote/Program.cs
+++ SchiffeUndBoote/Program.cs
@@ -2,11 +2,21 @@
 
 namespace SchiffeUndBoote
 {
-    class Program
+    public class Program
     {
-        static void main()
+        public static void Main()
         {
-            
+            Land china = new Land("China", "CH");
+            Land deutschland = new Land ("Deutschland", "D");
+            Land uk = new Land("United Kindgom", "UK");
+
+            Besitzer chef = new Besitzer("Sherlock", "Holmes", "Baker Street", "221B", "NW1", "London", uk);
+
+
+            Containerschiff c = new Containerschiff("Ever Given", china, 400,59, 33, 16, 20124, 59300);
+            Tankschiff t = new Tankschiff("CABO SOUNION", deutschland, 229, 32, 15, 8, 40000, 20000);
+            Segelboot falke = new Segelboot("Unsinkbar II", chef, 5, 3, 2, 1, 1);
+            Sportboot iltis = new Sportboot("Unsink-Bar", chef, 16, 5, 5, 3, 25);
         }
     }
 }
