67a9f5a763531fe88c369d67b212cb9cfa432a7d
M.Thomas <m.thomas@gso.schule.koeln>
2022-09-26 12:00:00 +0200

    .gitignore, Motorboot.cs und 4 weitere dateien aktualisiert...

--- SchiffeUndBoote/Program.cs
+++ SchiffeUndBoote/Program.cs
@@ -16,7 +16,7 @@ namespace SchiffeUndBoote
             Containerschiff c = new Containerschiff("Ever Given", china, 400,59, 33, 16, 20124, 59300);
             Tankschiff t = new Tankschiff("CABO SOUNION", deutschland, 229, 32, 15, 8, 40000, 20000);
             Segelboot falke = new Segelboot("Unsinkbar II", chef, 5, 3, 2, 1, 1);
-            Sportboot iltis = new Sportboot("Unsink-Bar", chef, 16, 5, 5, 3, 25);
+            Motorboot iltis = new Sportboot("Unsink-Bar", chef, 16, 5, 5, 3, 25);
         }
     }
 }
