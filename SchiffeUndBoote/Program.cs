ada8b5ebf17eaa0de6dcd497d510c859b46c7bb8
M.Thomas <m.thomas@gso.schule.koeln>
2024-03-04 08:54:41 +0100

    Beispiel für Polymorphie eingbaut

--- SchiffeUndBoote/Program.cs
+++ SchiffeUndBoote/Program.cs
@@ -4,6 +4,7 @@ namespace SchiffeUndBoote
 {
     public class Program
     {
+        
         public static void Main()
         {
             Land china = new Land("China", "CH");
@@ -12,11 +13,16 @@ namespace SchiffeUndBoote
 
             Besitzer chef = new Besitzer("Sherlock", "Holmes", "Baker Street", "221B", "NW1", "London", uk);
 
-
-            Containerschiff c = new Containerschiff("Ever Given", china, 400,59, 33, 16, 20124, 59300);
-            Tankschiff t = new Tankschiff("CABO SOUNION", deutschland, 229, 32, 15, 8, 40000, 20000);
-            Segelboot falke = new Segelboot("Unsinkbar II", chef, 5, 3, 2, 1, 5.0);
-            Motorboot iltis = new Motorboot("Unsink-Bar", chef, 16, 5, 5, 3, 25.0);
+            List<Wasserfahrzeug> wasserfahrzeuge = new List<Wasserfahrzeug>(); //Polymorphie!
+            wasserfahrzeuge.Add(new Containerschiff("Ever Given", china, 400,59, 33, 16, 20124, 59300));
+            wasserfahrzeuge.Add(new Tankschiff("CABO SOUNION", deutschland, 229, 32, 15, 8, 40000, 20000));
+            wasserfahrzeuge.Add(new Segelboot("Unsinkbar II", chef, 5, 3, 2, 1, 5.0));
+            wasserfahrzeuge.Add(new Motorboot("Unsink-Bar", chef, 16, 5, 5, 3, 25.0));
+  
+            foreach(Wasserfahrzeug w in wasserfahrzeuge)
+            {
+                Console.WriteLine(w.Name +"\t" + w.GetType().Name);
+            }
         }
     }
 }
